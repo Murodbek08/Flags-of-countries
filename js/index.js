fd73d395802a82d0fd1d45c7652c2baf10aa19d6
Murodbek08 <arslonovmurodbek071@gmail.com>
2025-06-02 19:54:54 +0500

    new element

--- js/index.js
+++ js/index.js
@@ -11,37 +11,17 @@ let loading = `
           <span class="bar"></span>
           <span class="bar bar2"></span>
         </div>
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 101 114"
-          class="svgIcon"
-        >
-          <circle
-            stroke-width="7"
-            stroke="black"
-            transform="rotate(36.0692 46.1726 46.1727)"
-            r="29.5497"
-            cy="46.1727"
-            cx="46.1726"
-          ></circle>
-          <line
-            stroke-width="7"
-            stroke="black"
-            y2="111.784"
-            x2="97.7088"
-            y1="67.7837"
-            x1="61.7089"
-          ></line>
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 101 114" class="svgIcon">
+          <circle stroke-width="7" stroke="black" transform="rotate(36.0692 46.1726 46.1727)" r="29.5497" cy="46.1727" cx="46.1726"></circle>
+          <line stroke-width="7" stroke="black" y2="111.784" x2="97.7088" y1="67.7837" x1="61.7089"></line>
         </svg>
       </div>
     </div>
   </div>
 `;
-
+let xhr = new XMLHttpRequest();
 function getData(url) {
   let pr = new Promise((resolve, reject) => {
-    let xhr = new XMLHttpRequest();
     xhr.onreadystatechange = () => {
       if (xhr.readyState === 4 && xhr.status === 200) {
         let countriesData = JSON.parse(xhr.response);
@@ -57,6 +37,7 @@ function getData(url) {
 }
 
 searchInput.addEventListener("keyup", function (el) {
+  activePage = 1;
   let search = el.target.value.trim().toLowerCase();
   if (search == "") {
     async function initPagination() {
@@ -140,20 +121,14 @@ initPagination();
 
 function renderPaginationButtons() {
   pagination.innerHTML = "";
-
-  pagination.innerHTML += `<button onclick="paginationItemBack()"> 
-    <img src="../images/chevron-left.svg" alt="Chevron icon">
-  </button>`;
+  pagination.innerHTML += `<button onclick="paginationItemBack()"><img src="../images/chevron-left.svg" alt="Chevron icon"></button>`;
 
   for (let i = 1; i <= totalPages; i++) {
     pagination.innerHTML += `<button class="${
       activePage === i ? "active" : ""
     }" onclick="paginationItem(${i})">${i}</button>`;
   }
-
-  pagination.innerHTML += `<button onclick="paginationItemNext()"> 
-    <img src="../images/chevron-right.svg" alt="Chevron icon">
-  </button>`;
+  pagination.innerHTML += `<button onclick="paginationItemNext()"><img src="../images/chevron-right.svg" alt="Chevron icon"></button>`;
 }
 
 function paginationItemBack() {
@@ -190,11 +165,19 @@ async function loadPage(page) {
   });
 }
 
-function countriesCard({ flags, name, population, region, capital }) {
+function countriesCard({ flags = {}, name, population, region, capital }) {
+  let img;
+  if (xhr.status !== 200) {
+    img = `https://upload.wikimedia.org/wikipedia/commons/a/a3/Image-not-found.png`;
+  } else if (Boolean(flags.png)) {
+    img = flags.png;
+  } else if (Boolean(flags.svg)) {
+    img = flags.svg;
+  }
   return `
     <div class="countries-card">
       <a href="../pages/flag.html?Id=${population}">
-        <img src="${flags.png}" alt="${flags.alt}" />
+       <img  src="${img}" alt="${flags.alt}"  loading="lazy" />
       </a>
       <div class="countries-text">
         <h3>${name.common}</h3>
--- js/main.js
+++ js/main.js
@@ -1,34 +1,21 @@
 let darkMode = document.querySelector(".dark-mode");
-
 let storage = localStorage.getItem("dark-mode");
+
 if (storage === "dark") {
   document.body.classList.add("dark");
-  darkMode.innerHTML = `
-    <img src="../images/sun.svg" alt="Sun icon ?" />
-    <span>Light Mode</span>
-  `;
+  darkMode.innerHTML = `<img src="../images/sun.svg" alt="Sun icon ?" /><span>Light Mode</span>`;
 } else {
   document.body.classList.remove("dark");
-  darkMode.innerHTML = `
-    <img src="../images/moon.svg" alt="Moon icon" />
-    <span>Dark Mode</span>
-  `;
+  darkMode.innerHTML = `<img src="../images/moon.svg" alt="Moon icon" /><span>Dark Mode</span>`;
 }
+
 darkMode.addEventListener("click", () => {
   document.body.classList.toggle("dark");
   if (document.body.classList.contains("dark")) {
     localStorage.setItem("dark-mode", "dark");
-    darkMode.innerHTML = `
-      <img src="../images/sun.svg" alt="Sun icon ?" />
-      <span>Light Mode</span>
-    `;
+    darkMode.innerHTML = `<img src="../images/sun.svg" alt="Sun icon ?" /><span>Light Mode</span>`;
   } else {
-    localStorage.setItem("dark-mode", "no-dark");
-    darkMode.innerHTML = `
-      <img src="../images/moon.svg" alt="Moon icon ?" />
-      <span>Dark Mode</span>
-    `;
+    localStorage.setItem("<dark-m></dark-m>ode", "no-dark");
+    darkMode.innerHTML = `<img src="../images/moon.svg" alt="Moon icon ?" /><span>Dark Mode</span>`;
   }
 });
-
-
